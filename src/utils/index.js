da264a5ec4be82d5652d0a2ec6743e77d915b327
fansubsbrasil <fansubsbrasil@gmail.com>
2022-05-24 16:03:04 -0300

    ajuste servepath

--- src/utils/index.js
+++ src/utils/index.js
@@ -227,5 +227,5 @@ export const getCalendarDays = (value) =>{
 }
 const dev = process.env.NODE_ENV !== 'production';
 //export const serverPath = dev ? 'http://localhost:3000' : 'https://bravox-app.herokuapp.com';
-//export const serverPath = 'https://extensao-bravox2-master.vercel.app'
-export const serverPath = 'http://localhost:3000'
+export const serverPath = 'https://extensao-bravox2-master.vercel.app'
+//export const serverPath = 'http://localhost:3000'
